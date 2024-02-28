604d04cb44c54777c3e96fcf27883d221437fddc
dekanym <153619521+dekanym@users.noreply.github.com>
2024-02-28 08:39:49 +0100

    dolgozat2.java

--- dolgozat2.java
+++ dolgozat2.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class dolgozat2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("kicsit néztem a math modult interneten");
-
+        
         System.out.println("kérem a kör sugarát: ");
         double radius = sc.nextDouble();
 
-        double area = Math.PI * Math.pow(radius, 2);
+        double area = Math.PI * (radius *radius);
 
         System.out.println("területe: "+ area);
 
