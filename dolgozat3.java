ab2b3eb86cb4a95e3f893facc1290a007f056143
dekanym <153619521+dekanym@users.noreply.github.com>
2024-02-28 08:35:48 +0100

    dolgozat3.java

--- dolgozat3.java
+++ dolgozat3.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner6;
-
 public class dolgozat3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
