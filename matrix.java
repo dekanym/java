3b24a1bae1b4510b6a7dc8b8e202d61089a19863
dekanym <153619521+dekanym@users.noreply.github.com>
2024-03-12 14:17:35 +0100

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,10 @@
+public class matrix {
+    public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {  
+        for (int j = 1; j <= 4; j++) {
+          System.out.print(i+ ". sor " +j+". oszlop ");
+        }
+        System.out.println();
+      }
+    }
+  }
