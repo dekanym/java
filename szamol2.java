517bb43ed41b28e99d46ddeecfd194fae71e8b7b
dekanym <153619521+dekanym@users.noreply.github.com>
2024-03-13 08:26:52 +0100

    Add files via upload

---
+++ szamol2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class szamol2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("kérek egy számot: ");
+        int myint1 = sc.nextInt();
+        System.out.println(); 
+        for (int i = 0; i <= 100; i++) {
+            if (i % myint1 == 0) {
+                System.out.println(i);
+            }
+            else if (i == myint1 * 10) {
+                break;}
+        }
+    }
+}
