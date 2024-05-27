61e6c07a13f1c05094947c4bf9819049e68aa64d
dekanym <153619521+dekanym@users.noreply.github.com>
2024-05-27 12:13:31 +0200

    DataStructuresDolgozat.java

--- DataStructuresDolgozat.java
+++ DataStructuresDolgozat.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class DataStructuresDolgozat {
 
+    private static Map<String, String> capitals = new HashMap<>();
+
     public static void main(String[] args) {
 
         // Hozz létre egy változót, mely fővárosok neveit tárolja.
@@ -41,65 +43,61 @@ public class DataStructuresDolgozat {
         // Hozz létre egy változót, mely főváros, ország párokat tartalmaz.
         // 1 pont
 
-      
         // A tartalma a főváros, ország pároknak a következő legyen:
         // "Wien", "Österreich"
         // "Budapest", "Magyarország"
         // "Ljubljana", "Slovenija"
         // 3 pont
-
-        private static Map<String, String> capitals = new HashMap<>();
-
         capitals.put("Wien", "Österreich");
         capitals.put("Budapest", "Magyarország");
         capitals.put("Ljubljana", "Slovenija");
-    
 
         // Írasd ki a főváros, ország párokat!
         // 1 pont
         kiirParok();
 
-
         // Írad ki a fovarosOrszaga() metódussal, hogy mely ország fővárosa "Budapest"
         // 1 pont
-
         fovarosOrszaga("Budapest");
 
         // Írad ki a fovarosOrszaga() metódussal, hogy mely ország fővárosa "Zagreb"
         // 1 pont
         fovarosOrszaga("Zagreb");
 
-        
-        // Print out the country of "Budapest"
-        
-        // Print out the country of "Zagreb"
-
         // Az ujFovarosOrszagPar() metódussal add hozzá a "Zagreb", "Hrvatska" párt a fővárosok országok párokhoz
         // 1 pont
-
         ujFovarosOrszagPar("Zagreb", "Hrvatska");
 
-        // A torolAdatPart() metódussal törld a "Ljubljana" főváros ország párt
+        // A torolAdatPart() metódussal töröld a "Ljubljana" főváros ország párt
         // 1 pont
         torolAdatPart("Ljubljana");
 
-
-        // A torolAdatPart() metódussal törld a "Prag" főváros ország párt
+        // A torolAdatPart() metódussal töröld a "Prag" főváros ország párt
         // kezeld a felmerülő hibát és írd ki a hibát
         // 3 pont
         torolAdatPart("Prag");
-
 
         // Írasd ki a főváros, ország párokat!
         // 1 pont
         kiirParok();
     }
+
+    public static void ujElemHozzaadasa(ArrayList<String> cities) {
+        cities.add("Ljubljana");
+        cities.add("Sarajevo");
+        cities.add("Zagreb");
+    }
+
+    public static void torolElemet(ArrayList<String> cities) {
+        cities.remove("Sarajevo");
+    }
+
     public static void kiirParok() {
         for (Map.Entry<String, String> entry : capitals.entrySet()) {
             System.out.println(entry.getKey() + ", " + entry.getValue());
         }
     }
-    
+
     public static void fovarosOrszaga(String fovaros) {
         String orszag = capitals.get(fovaros);
         if (orszag != null) {
@@ -108,10 +106,17 @@ public class DataStructuresDolgozat {
             System.out.println("Nem található információ " + fovaros + " fővárosáról.");
         }
     }
-    
+
     public static void ujFovarosOrszagPar(String fovaros, String orszag) {
         capitals.put(fovaros, orszag);
     }
+
+    public static void torolAdatPart(String fovaros) {
+        if (capitals.containsKey(fovaros)) {
+            capitals.remove(fovaros);
+        } 
+    }
+
     
     /**
      * ujElemHozzaadasa() - egy elemet ad hozzá a megadott listához
